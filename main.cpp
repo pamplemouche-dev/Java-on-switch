32fb313de4194ecfd65ec21033cbf062f2bc76dc
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-03-06 15:03:35 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,36 +1,14 @@
 #include <switch.h>
-#include <jni.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
 #include <string>
 #include <vector>
-#include <cstring> // Ajout crucial pour strstr
+#include <cstring>
 
-// Sécurité pour la version JNI si non définie par le header
-#ifndef JNI_VERSION_17
-#define JNI_VERSION_17 0x00110000
-#endif
-
-std::string buildClasspath() {
-    std::string cp = "-Djava.class.path=";
-    DIR* dir = opendir("romfs:/mods");
-    struct dirent* ent;
-    bool first = true;
-
-    if (dir) {
-        while ((ent = readdir(dir)) != NULL) {
-            if (strstr(ent->d_name, ".jar")) {
-                if (!first) cp += ":";
-                cp += "romfs:/mods/";
-                cp += ent->d_name;
-                first = false;
-            }
-        }
-        closedir(dir);
-    }
-    return cp;
-}
+// --- LIAISON AVEC LE BOT ---
+// Cette fonction sera créée par translator.py et injectée dans minecraft_server_native.cpp
+extern "C" void start_minecraft_native_core();
 
 int main(int argc, char* argv[]) {
     consoleInit(NULL);
@@ -39,43 +17,46 @@ int main(int argc, char* argv[]) {
     PadState pad;
     padInitializeDefault(&pad);
 
-    printf("\x1b[1;32m--- Minecraft Switch Launcher ---\x1b[0m\n");
+    printf("\x1b[1;32m--- Minecraft Switch AOT Launcher ---\x1b[0m\n");
+    printf("Mode : Natif (AOT) + Mods SD\n\n");
 
-    std::string classpath = buildClasspath();
-    
-    JavaVM *jvm;
-    JNIEnv *env;
-    JavaVMInitArgs vm_args;
-    JavaVMOption options[4];
+    // 1. DÉMARRAGE DU CŒUR TRADUIT
+    // C'est instantané car le Bot GitHub a déjà fait le travail
+    printf("Chargement du moteur traduit...\n");
+    start_minecraft_native_core();
 
-    options[0].optionString = (char*)classpath.c_str();
-    options[1].optionString = (char*)"-Xmx2G";
-    options[2].optionString = (char*)"-XX:+UseG1GC";
-    options[3].optionString = (char*)"-Xverify:none";
-
-    vm_args.version = JNI_VERSION_17; 
-    vm_args.nOptions = 4;
-    vm_args.options = options;
-    vm_args.ignoreUnrecognized = JNI_FALSE;
-
-    printf("Lancement de la JVM...\n");
-
-    jint res = JNI_CreateJavaVM(&jvm, (void**)&env, &vm_args);
-
-    if (res == JNI_OK) {
-        printf("Java est pret. Lancement de Forge...\n");
+    // 2. SCAN DES MODS SUR LA CARTE SD
+    // On cherche dans sdmc:/switch/mods/ pour ne pas alourdir le NRO
+    printf("Recherche de mods sur SD (sdmc:/switch/mods/)... \n");
+    DIR* dir = opendir("sdmc:/switch/mods/");
+    if (dir) {
+        struct dirent* ent;
+        int modCount = 0;
+        while ((ent = readdir(dir)) != NULL) {
+            if (strstr(ent->d_name, ".jar")) {
+                printf(" - \x1b[1;34mMod detecte\x1b[0m : %s\n", ent->d_name);
+                modCount++;
+                // Ici, on passera plus tard le chemin au moteur pour le chargement dynamique
+            }
+        }
+        closedir(dir);
+        if(modCount == 0) printf("Aucun mod trouve.\n");
     } else {
-        printf("\x1b[1;31mErreur JVM : %d\x1b[0m\n", res);
+        printf("\x1b[1;33mDossier /switch/mods/ absent sur la SD.\x1b[0m\n");
     }
+
+    printf("\n\x1b[1;32mPret ! Appuyez sur + pour quitter.\x1b[0m\n");
 
     while (appletMainLoop()) {
         padUpdate(&pad);
         u64 kDown = padGetButtonsDown(&pad);
-        if ((kDown & HidNpadButton_Plus) && (kDown & HidNpadButton_Minus)) break;
+
+        // Sortie avec le bouton Plus
+        if (kDown & HidNpadButton_Plus) break;
+
         consoleUpdate(NULL);
     }
 
-    if (res == JNI_OK) jvm->DestroyJavaVM();
     romfsExit();
     consoleExit(NULL);
     return 0;
