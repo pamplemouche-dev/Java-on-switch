8efad379ec77743e5ef4379fbdf7b3c2b02b0887
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-03-09 06:38:27 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,13 +3,11 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <string>
-#include <vector>
 #include <cstring>
 
-// Cette fonction est générée dynamiquement par le bot dans minecraft_server_native.cpp
+// Appel au moteur traduit par le bot
 extern "C" void start_minecraft_native_core();
 
-// Fonction pour scanner les mods sur la carte SD
 void scanSDMods() {
     printf("\x1b[1;34m[SD Card]\x1b[0m Recherche de mods dans sdmc:/switch/mods/...\n");
     DIR* dir = opendir("sdmc:/switch/mods/");
@@ -22,7 +20,7 @@ void scanSDMods() {
         }
         closedir(dir);
     } else {
-        printf("\x1b[1;31m[Erreur]\x1b[0m Dossier /switch/mods/ introuvable sur la SD.\n");
+        printf("\x1b[1;33m[Info]\x1b[0m Aucun dossier /switch/mods/ sur la SD.\n");
     }
 }
 
@@ -34,13 +32,11 @@ int main(int argc, char* argv[]) {
     padInitializeDefault(&pad);
 
     printf("\x1b[1;32m--- Minecraft Switch Native Edition ---\x1b[0m\n");
-    printf("Statut : Moteur traduit par Bot AOT\n\n");
-
-    // 1. Lancer le coeur du jeu (déjà traduit en C++)
-    printf("Initialisation du coeur de Minecraft...\n");
+    
+    // Lancement du coeur traduit
     start_minecraft_native_core();
 
-    // 2. Scanner les mods sur la SD
+    // Scan des mods sur la SD (Accepte les mods sans les traduire)
     scanSDMods();
 
     printf("\n\x1b[1;32mPret ! Appuyez sur + pour quitter.\x1b[0m\n");
@@ -48,9 +44,7 @@ int main(int argc, char* argv[]) {
     while (appletMainLoop()) {
         padUpdate(&pad);
         u64 kDown = padGetButtonsDown(&pad);
-
         if (kDown & HidNpadButton_Plus) break;
-
         consoleUpdate(NULL);
     }
 
